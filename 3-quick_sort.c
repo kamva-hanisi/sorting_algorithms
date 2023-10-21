a6cd2c8f3c3bc303e811a14abd75788a704255cd
kamva-hanisi <lucashanisi@gmail.com>
2023-10-21 03:45:23 -0700

    amazing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -28,31 +28,25 @@ int partition(int *array, int low, int high, int size)
 {
 	int pivot = array[high];
 	int i = low - 1;
-	int j;
+	int j; temp;
 
 	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
-
-
-			if (i != j)
-			{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
 
 			print_array(array, size);
-			}
 		}
 	}
 
-	swap(&array[i + 1], &array[high]);
-
-	if (i + 1 != high)
-	{
-
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
 	print_array(array, size);
-	}
 
 	return (i + 1);
 }
