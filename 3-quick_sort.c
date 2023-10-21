698406599cf1dcabea7451b6507489ac85057e22
kamva-hanisi <lucashanisi@gmail.com>
2023-10-21 03:19:29 -0700

    quick sort array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -35,18 +35,22 @@ int partition(int *array, int low, int high, int size)
 		if (array[j] < pivot)
 		{
 			i++;
+			swap(&array[i], &array[j]);
+
 
 			if (i != j)
 			{
-			swap(&array[i], &array[j]);
+
 			print_array(array, size);
 			}
 		}
 	}
 
+	swap(&array[i + 1], &array[high]);
+
 	if (i + 1 != high)
 	{
-	swap(&array[i + 1], &array[high]);
+
 	print_array(array, size);
 	}
 
