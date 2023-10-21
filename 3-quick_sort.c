b8e94518aa9d8706242194c71c597c20a5bde01c
kamva-hanisi <lucashanisi@gmail.com>
2023-10-21 04:15:46 -0700

    checking code

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -28,9 +28,9 @@ int partition(int *array, int low, int high, int size)
 {
 	int pivot = array[high];
 	int i = low - 1;
-	int j; temp;
+	int j, temp;
 
-	for (j = low; j <= high - 1; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < pivot)
 		{
@@ -50,11 +50,11 @@ int partition(int *array, int low, int high, int size)
 	temp = array[i + 1];
 	array[i + 1] = array[high];
 	array[high] = temp;
-	
+
 	if (i + 1 != high)
-		{
-			print_array(array, size);
-		}
+	{
+		print_array(array, size);
+	}
 
 	return (i + 1);
 }
